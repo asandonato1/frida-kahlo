845a08c9994392e1b7475781afccacbfca1788de
maze1337 <lockdead456@gmail.com>
2020-11-20 15:19:26 -0300

    Create project.py

---
+++ project.py
@@ -0,0 +1,18 @@
+paintings = ["The Two Fridas", "My Dress Hangs Here", "Tree of Hope", "Self Portrait With Monkeys"]
+dates = [1939, 1933, 1946, 1940, 1944, 1946, 1937]
+
+paintings = list(zip(paintings, dates))
+paintings.append(('The Broken Column', 1944))
+paintings.append(('The Wounded Deer', 1946))
+paintings.append(('Me and My Doll', 1937))
+
+
+print(paintings)
+
+len(paintings)
+audio_tour_number = [i for i in range(1, 8)]
+
+print(audio_tour_number)
+
+master_list = list(zip(paintings, audio_tour_number))
+print(master_list)
